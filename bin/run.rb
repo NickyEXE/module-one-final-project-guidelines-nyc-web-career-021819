5fb738c0b780d2a623240213338ab10777f342d7
NickyEXE <dovern42@gmail.com>
2019-03-04 17:04:11 -0500

    here's our run.rb with our eventual user map

--- bin/run.rb
+++ bin/run.rb
@@ -7,3 +7,12 @@ location = get_location_from_user
 hash = find_karaoke_places_near_location(location)
 puts hash
 puts "HELLO WORLD"
+
+
+#WHERE YOU AT?
+#Here are 5 karaoke places by you.
+##EVENTUALLY we should which you've sung at
+#Do you want to sing at any of these?
+#if no -- then it prompts you "find five more?" or "add new location"
+#if yes, which of these do you want to hit up?
+#welcome to NAME, "Do you want to add a song?" "Do you want to see your saved songs?"
