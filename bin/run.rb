5896d5ee1642c51658c93457e70f2d3bafcf0682
NickyEXE <dovern42@gmail.com>
2019-03-07 18:09:55 -0500

    done

--- bin/run.rb
+++ bin/run.rb
@@ -1,9 +1,25 @@
 require_relative '../config/environment'
 
 #welcomes the user
+system "clear"
+ puts "\e[H\e[2J"
 
 def runner
   welcome
   ask_how_they_want_to_search
 end
 runner
+
+sleep(1)
+puts "leaving."
+sleep(1)
+puts "leaving..."
+sleep(1)
+puts "leaving....."
+sleep(1)
+puts "leaving......"
+sleep(1)
+puts "goodbye!"
+sleep(2)
+system "clear"
+ puts "\e[H\e[2J"
