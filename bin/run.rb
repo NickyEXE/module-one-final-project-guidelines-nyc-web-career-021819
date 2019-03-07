bad815e07f5df804870c8281fc1a4eecf3ca013f
NickyEXE <dovern42@gmail.com>
2019-03-07 12:06:30 -0500

    added find-by-name functionality

--- bin/run.rb
+++ bin/run.rb
@@ -1,35 +1,9 @@
 require_relative '../config/environment'
-# require_relative "../cli/api_communicator.rb"
-# require_relative "../cli/CLI.rb"
-# require_relative "../cli/print_and_pick.rb"
-# require_relative "../cli/in_karaoke_place.rb"
 
 #welcomes the user
 def runner
   welcome
   ask_how_they_want_to_search
-  #get_location_from_user asks the user where they want to sing,
-  #Gets.chomp the location (a string) from the user,
-  #stores it as a variable named "location" in run.rb
-  #karaoke_place = print_and_pick(business_hashes,0)
-
-  #Including this so that it ends successfully if the user does not pick a place
-  # if karaoke_place.class == KaraokePlace
-  #   input = karaoke_place_welcome(karaoke_place)
-  #
-  # end
 end
 
 runner
-
-
-binding.pry
-#WHERE YOU AT? DONE
-#Here are 5 karaoke places by you. DONE
-##EVENTUALLY we should which you've sung at
-#Do you want to sing at any of these?
-
-
-#if no -- then it prompts you "find five more?" or "add new location"
-#if yes, which of these do you want to hit up? DONE
-#welcome to NAME, "Do you want to add a song?" "Do you want to see your saved songs?"
--- cli/find_by_name_or_location.rb
+++ cli/find_by_name_or_location.rb
@@ -19,12 +19,68 @@ def ask_how_they_want_to_search
   end
 end
 
+def karaoke_spot_find_by_name
+  puts "What's the name of the karaoke place you want to go to?"
+  input1 = gets.chomp()
+  if input1.downcase == "exit"
+    puts "Exiting program."
+  else
+    puts "Where is #{input1}? (Enter a city)"
+    input2 = gets.chomp()
+    hash = search_yelp_by_name(input1, input2)
+    clean_the_named_hash(hash)
+  end
+end
+
+
+def clean_the_named_hash(hash)
+  if hash.key?("error")
+    puts "I hit an error. Are you sure you entered that correctly?"
+    puts "Sending you back to confirm how you want to search."
+    ask_how_they_want_to_search
+  elsif hash["businesses"].length > 0
+    #We should confirm it's the right one.
+    karaoke_place = picker(hash["businesses"],0)
+    check_if_it_is_the_right_place(karaoke_place)
+  else
+    puts "I hit an error. Are you sure you entered that correctly?"
+    puts "Sending you back to confirm how you want to search."
+    ask_how_they_want_to_search
+  end
+end
+
+def check_if_it_is_the_right_place(karaoke_place)
+  puts "
+  We found the following place:"
+  puts karaoke_place.name
+  puts karaoke_place.location.split("**")
+  puts "
+  Is this where you want to go?
+  Y/N or type EXIT to leave the program or BACK to go to the main menu."
+  input = gets.chomp()
+  case input
+  when "Y"
+    puts "Going to #{karaoke_place.name}."
+    karaoke_place_welcome(karaoke_place)
+  when "N"
+    karaoke_spot_find_by_name
+  when "back" || "Back" || "BACK"
+    ask_how_they_want_to_search
+  when "exit" || "Exit" || "EXIT"
+    puts "I hope you find what you're looking for. Goodbye."
+  else
+    puts "That was an invalid input. Try again!"
+    check_if_it_is_the_right_place(karaoke_place)
+  end
+end
+
+
+
 def karaoke_spot_find_by_location
   #get_location_from_user asks the user where they want to sing,
   #Gets.chomp the location (a string) from the user,
   #stores it as a variable named "location" in run.rb
   #karaoke_place = print_and_pick(business_hashes,0)
-
 
   location = get_location_from_user
 
@@ -33,7 +89,6 @@ def karaoke_spot_find_by_location
   #Pulls a raw_hash of businesses from the Yelp API for Karaoke
   #in the location provided by the user
   raw_hash = find_karaoke_places_near_location(location)
-
   #Cleans the hash, makes it an array of businesses, and sorts by rating
   business_hashes = produce_cleaned_array_of_businesses_from_yelp_raw_hash_sorted_by_rating(raw_hash)
     if business_hashes.length > 0
